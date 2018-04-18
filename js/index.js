c739dc2aa71f0451176a4075a478d8d38bedeee6
gentlemanboy <igorsennin@gmail.com>
2018-04-18 19:18:24 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+$('.navAgenda').click(function(){
+    $('.agenda').fadeToggle('slow');
+});
+
+ $('.testebutton').click(function() {
+            $('body').load('system.html');
+        });
